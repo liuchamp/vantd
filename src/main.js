690b0597e1d0d5e0159be6febb954e6d0d6e2477
yexingyun <yexingyun9210@gmail.com>
2019-06-11 20:42:00 +0700

    feat: 🎸 开始开发vue界面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import { Button } from "ant-design-vue";
+Vue.component(Button.name, Button);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
